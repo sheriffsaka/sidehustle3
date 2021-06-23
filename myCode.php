6ae69e27ec48b49bb651d8d1d6050e0bd985c9ce
Saka Sheriff A <sheriffdeenalade@gmail.com>
2021-06-23 22:44:49 +0100

    My First File SH-IT-32931
    My Hello World PHP File. Saka Sheriff Alade

---
+++ myCode.php
@@ -0,0 +1,12 @@
+<?php
+    echo("Hello World...");
+
+    $hello = "Hello World1...";
+    $age = 24;
+    $name = "I am Saka Sheriff";
+
+    echo ("{$name}" ."".$hello." I am"."{$age}"." years old."); 
+    echo ($name ."".$hello." I am".$age." years old.");
+    // My name is Saka SHeriff Alade with Intern ID: SH-IT-32931.
+
+?>
